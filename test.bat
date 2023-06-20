a16a2b6f0ae407c0199509b8214eabb494fc9474
bruce <bruce.payne@dynetics.com>
2023-06-20 14:44:49 -0500

    Preseason

---
+++ test.bat
@@ -0,0 +1,5 @@
+if "%1" == "" (
+  echo arg was blank
+) else (
+  echo arg was "%1"
+)
--- upload.bat
+++ upload.bat
@@ -1,4 +1,7 @@
-git add --all
-git commit -m "%1"
-git push
-
+if "%1" == "" (
+  echo Usage: upload VERSION
+) else (
+  git add --all
+  git commit -m "%1"
+  git push
+)
